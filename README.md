e55d373168370ab74aa6986650c8ffd84ad7bf76
Tibor Hudik <tibor@barrage.net>
2022-04-27 08:59:14 +0200

    Upgraded to diesel 2.0 version; Simplified API and exposed only one trait;

--- README.md
+++ README.md
@@ -1,83 +1,9 @@
-[![Crates.io](https://img.shields.io/crates/v/lenght_aware_paginator.svg)](https://crates.io/crates/lenght_aware_paginator)
-
-# lenght_aware_paginator
-
-```toml
-lenght_aware_paginator = "0.1.0"
-```
-
-Lenght aware paginator enables you to paginate Diesel queries and have information about the lenght
-of data being paginated. It will give you total number of items, and last page that you can navigate to
-and still get some kind of data.
-
-You will only have to provide page and per_page parameters.
-
-```rust
-use diesel::pg::PgConnection;
-use diesel::Connection;
-use diesel::QueryDsl;
-use lenght_aware_paginator::{LoadPaginated, Response};
-use serde::{Deserialize, Serialize};
-
-// user.rs : your model for the table represented in schema.rs
-#[derive(Queryable, Deserialize, Serialize)]
-pub struct User {
-    id: i32,
-    email: String,
-    first_name: String,
-    last_name: String,
-    password: String,
-}
-
-fn get_connection() -> PgConnection {
-    let database_url =
-        dotenv::var("DATABASE_URL").expect("You have to provide DATABASE_URL to run tests");
-
-    PgConnection::establish(&database_url)
-        .unwrap_or_else(|_| panic!("Error connecting to {}", &database_url))
-}
-
-#[test]
-fn test_orm_query_pagination() {
-    let connection = get_connection();
-
-    // Use `lenght_aware_paginator::LoadPaginated` trait to enable
-    // using the `load_paginated` method on your query.
-    // Your query will return `lenght_aware_paginator::Response<T>` struct
-    let response: Response<User> = schema::users::table
-        .into_boxed()
-        .load_paginated(connection, page, per_page)
-        .unwrap();
-
-    assert_eq!(response.page, 1);
-    assert_eq!(response.per_page, 10);
-    assert_eq!(response.total, 15);
-    assert_eq!(response.last_page, 2);
-    assert_eq!(response.data.len(), 10);
-}
-```
-
-## License
-
-Licensed under either of
-
-* Apache License, Version 2.0, ([LICENSE-APACHE](LICENSE-APACHE) or https://www.apache.org/licenses/LICENSE-2.0)
-* MIT license ([LICENSE-MIT](LICENSE-MIT) or https://opensource.org/licenses/MIT)
-
-at your option.
-
-### Contribution
-
-Unless you explicitly state otherwise, any contribution intentionally
-submitted for inclusion in the work by you, as defined in the Apache-2.0
-license, shall be dual licensed as above, without any additional terms or
-conditions.
 [![Crates.io](https://img.shields.io/crates/v/length_aware_paginator.svg)](https://crates.io/crates/length_aware_paginator)
 
 # length_aware_paginator
 
 ```toml
-length_aware_paginator = "0.1.0"
+length_aware_paginator = "1.0.0"
 ```
 
 Length aware paginator enables you to paginate Diesel queries and have information about the length
@@ -90,7 +16,7 @@ You will only have to provide page and per_page parameters.
 use diesel::pg::PgConnection;
 use diesel::Connection;
 use diesel::QueryDsl;
-use length_aware_paginator::{LoadPaginated, Response};
+use length_aware_paginator::{Paginate, Response};
 use serde::{Deserialize, Serialize};
 
 /// Get the database connection
@@ -126,14 +52,16 @@ pub struct User {
 
 #[test]
 fn test_orm_query_pagination() {
-    let connection = get_connection();
+    let mut connection = get_connection();
 
     // Use `length_aware_paginator::LoadPaginated` trait to enable
     // using the `load_paginated` method on your query.
     // Your query will return `length_aware_paginator::Response<T>` struct
     let response: Response<User> = schema::users::table
         .into_boxed()
-        .load_paginated(connection, page, per_page)
+        .page(Some(1))
+        .per_page(Some(10))
+        .load_paginated(&mut connection)
         .unwrap();
 
     assert_eq!(response.page, 1);
@@ -143,11 +71,6 @@ fn test_orm_query_pagination() {
     assert_eq!(response.data.len(), 10);
 }
 ```
-
-#### Limitations
-
-Unfortunatelly this is still not implemented to work with `sql_query()` due to its own limitations.
-Maybe in the future I'll update this package to enable this.
 
 ## License
 
--- src/traits.rs
+++ src/traits.rs
@@ -3,34 +3,53 @@ use diesel::pg::Pg;
 use diesel::prelude::*;
 use diesel::query_builder::*;
 use diesel::query_dsl::methods::LoadQuery;
-use diesel::sql_types::{BigInt, HasSqlType};
+use diesel::sql_types::BigInt;
 
 pub trait Paginate: Sized {
-    fn page(self, page: i64) -> PaginatedQuery<Self>;
-}
+    /// Start the pagination process by setting the page number
+    fn page(self, page: Option<i64>) -> PaginatedQuery<Self> {
+        let mut page = page.unwrap_or(1);
 
-impl<T> Paginate for T {
-    fn page(self, page: i64) -> PaginatedQuery<Self> {
+        if page <= 0 {
+            page = 1;
+        }
+
         PaginatedQuery {
             query: self,
-            per_page: 15,
             page,
+            per_page: 15,
+        }
+    }
+
+    /// Start the pagination process by setting the amount of items per page
+    fn per_page(self, per_page: Option<i64>) -> PaginatedQuery<Self> {
+        let mut per_page = per_page.unwrap_or(15);
+
+        if per_page <= 0 {
+            per_page = 15;
+        }
+
+        PaginatedQuery {
+            query: self,
+            page: 1,
+            per_page,
         }
     }
 }
+
+impl<T> Paginate for T {}
 
 impl<T> QueryFragment<Pg> for PaginatedQuery<T>
 where
     T: QueryFragment<Pg>,
 {
-    fn walk_ast(&self, mut out: AstPass<Pg>) -> QueryResult<()> {
+    fn walk_ast<'query>(&'query self, mut out: AstPass<'_, 'query, Pg>) -> QueryResult<()> {
         out.push_sql("SELECT *, COUNT(*) OVER () FROM (");
         self.query.walk_ast(out.reborrow())?;
         out.push_sql(") t LIMIT ");
         out.push_bind_param::<BigInt, _>(&self.per_page)?;
-        out.push_sql(" OFFSET ");
         let offset = (self.page - 1) * self.per_page;
-        out.push_bind_param::<BigInt, _>(&offset)?;
+        out.push_sql(format!(" OFFSET {}", offset).as_str());
         Ok(())
     }
 }
@@ -42,15 +61,32 @@ impl<T: Query> Query for PaginatedQuery<T> {
 impl<T> RunQueryDsl<PgConnection> for PaginatedQuery<T> {}
 
 impl<T> PaginatedQuery<T> {
+    /// Set page number on the query
+    pub fn page(self, page: Option<i64>) -> Self {
+        let mut page = page.unwrap_or(1);
+
+        if page <= 0 {
+            page = 1;
+        }
+
+        PaginatedQuery { page, ..self }
+    }
+
     /// Set per page size on the query
-    pub fn per_page(self, per_page: i64) -> Self {
+    pub fn per_page(self, per_page: Option<i64>) -> Self {
+        let mut per_page = per_page.unwrap_or(15);
+
+        if per_page <= 0 {
+            per_page = 15;
+        }
+
         PaginatedQuery { per_page, ..self }
     }
 
-    /// Load paginated data
-    pub fn load_paginated<U>(self, conn: &PgConnection) -> QueryResult<Response<U>>
+    /// Load paginated data with set page and per_page values
+    pub fn paginate<'query, U>(self, conn: &mut PgConnection) -> QueryResult<Response<U>>
     where
-        Self: LoadQuery<PgConnection, (U, i64)>,
+        Self: LoadQuery<'query, PgConnection, (U, i64)>,
     {
         let page = self.page;
         let per_page = self.per_page;
@@ -70,40 +106,5 @@ impl<T> PaginatedQuery<T> {
             last_page,
             data,
         })
-    }
-}
-
-pub trait LoadPaginated<U>:
-    Query + QueryId + QueryFragment<Pg> + LoadQuery<PgConnection, U>
-{
-    fn load_paginated(
-        self,
-        conn: &PgConnection,
-        page: Option<i64>,
-        per_page: Option<i64>,
-    ) -> QueryResult<Response<U>>;
-}
-
-impl<T, U> LoadPaginated<U> for T
-where
-    Self: Query + QueryId + QueryFragment<Pg> + LoadQuery<PgConnection, U>,
-    U: Queryable<Self::SqlType, Pg>,
-    Pg: HasSqlType<Self::SqlType>,
-{
-    fn load_paginated(
-        self,
-        conn: &PgConnection,
-        page: Option<i64>,
-        per_page: Option<i64>,
-    ) -> QueryResult<Response<U>> {
-        let page = page.unwrap_or(1);
-
-        let mut query = self.page(page);
-
-        if let Some(per_page) = per_page {
-            query = query.per_page(per_page);
-        }
-
-        query.load_paginated::<U>(conn)
     }
 }
